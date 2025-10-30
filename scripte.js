993ccb359554899cbd202c24b7f06b29819d2263
youssef boualy <youssefboua913@gmail.com>
2025-10-31 00:01:25 +0100

    ajouter js pour slectioner un evenment a 1er page

--- scripte.js
+++ scripte.js
@@ -6,10 +6,16 @@ article.forEach(art => {
     article.forEach(el => {
         el.classList.remove("bordercolor");
     });
-     art.addEventListener('click',()=>{
+    
 
      art.classList.add("bordercolor");
-    });
+    const evenement =art.children[0].textContent;
+    const date =art.children[1].textContent;
+    const ville =art.children[2].textContent;
+    const prix=art.children[3].textContent;
+    const place =art.children[4].textContent;
+   
+    
 });
 });
 
@@ -19,8 +25,10 @@ function suivant(t){
     let    section1=document.getElementById("s1");
     section1.style.display='none';
     section2.style.display='flex';
-  
-      }
+
+    
+ }
+
 
 
     
