b52dc14d507d6a22656d3b926b8bcb57cc928ede
youssef boualy <youssefboua913@gmail.com>
2025-10-30 21:07:59 +0100

    ajouter la function suivant et selectionert en js

--- scripte.js
+++ scripte.js
@@ -1,7 +1,16 @@
 
-function changebordere(t){
-          t.classList.toggle("bordercolor");
-           
+
+function slectionert(index){
+  let select = document.getElementById(index);
+  select.classList.toggle("bordercolor");
+    
+}
+
+function suivant(t){
+    let    section2=document.getElementById("s2");
+    let    section1=document.getElementById("s1");
+    section1.style.display='none';
+    section2.style.display='flex';
   
       }
 
